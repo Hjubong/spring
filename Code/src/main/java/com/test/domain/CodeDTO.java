dffa90fa65b26091a15c55606c523354867f1892
Hjubong <juju814@naver.com>
2023-11-24 17:23:40 +0900

    2023-11-24

---
+++ Code/src/main/java/com/test/domain/CodeDTO.java
@@ -0,0 +1,15 @@
+package com.test.domain;
+
+import lombok.Data;
+
+@Data
+public class CodeDTO {
+
+	private String seq;
+	private String subject;
+	private String code;
+	private String regdate;
+	private String language;
+	
+}
+
